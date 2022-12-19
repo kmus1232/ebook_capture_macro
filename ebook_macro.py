806152f2c383abe5d3188698aa86ffe97239bfec
kim_minseok <kmus1232@gmail.com>
2022-12-19 13:28:55 +0900

    사용성 개선

--- ebook_macro.py
+++ ebook_macro.py
@@ -10,15 +10,15 @@ os.mkdir(folderLocation)
 print("페이지 수를 입력하세요 : ", end='')
 pageCount = int(input())
 
-print("캡처할 범위의 왼쪽 상단으로 마우스를 이동해주세요")
-for i in range(3):
-    print(i)
+print("캡처할 범위의 왼쪽 상단으로 마우스를 이동해주세요, 5초 후 위치가 입력됩니다.")
+for i in range(5):
+    print(i + 1)
     time.sleep(1)
 leftTop = pag.position()
 
-print("캡처할 범위의 오른쪽 하단으로 마우스를 이동해주세요")
-for i in range(3):
-    print(i)
+print("캡처할 범위의 오른쪽 하단으로 마우스를 이동해주세요, 5초 후 위치가 입력됩니다.")
+for i in range(5):
+    print(i + 1)
     time.sleep(1)
 rightBottom = pag.position()
 
@@ -29,9 +29,9 @@ for i in range(pageCount):
     pag.screenshot(f"{folderLocation}/{i+1}.png",
                    region=(leftTop[0] * 2, leftTop[1] * 2, width * 2, height * 2))
     # mac은 고해상도를 위해 한 픽셀에 4개를 집어넣음, 그래서 2배 곱해줘야 함
-    pag.click(rightBottom)
-    time.sleep(0.5)
+    pag.press('right')
     print(f"page : {i+1}")
+    time.sleep(0.1)
 
-print("캡쳐 완료")
+print("스캔 완료")
 
